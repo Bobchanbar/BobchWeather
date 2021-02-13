abbf3d503e242160da3b0c1a0f98ef751f82d262
Bobchanbar <72271113+Bobchanbar@users.noreply.github.com>
2021-02-13 16:18:01 +0300

    Initial Commit

---
+++ BobchWeather/Modules/MainWeather/MainWeatherViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainWeatherViewController.swift
+//  BobchWeather
+//
+//  Created by Vladimir Barus on 13.02.2021.
+//
+
+import UIKit
+
+class MainWeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
